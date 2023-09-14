b1dcecdc498559739dc8006822570d009b71a06f
perseus-algol <perseus.algol2@gmail.com>
2023-09-14 13:38:50 +0300

    Add Stream param. Sender and workflow updated correspondingly. So now we can make several tests and have separated logs for them.

--- sender/src/index.ts
+++ sender/src/index.ts
@@ -10,8 +10,14 @@ function getRandomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-async function send_log(data: string): Promise<void> {
-  const response = await fetch('http://localhost:5080/api/org1/stream1/_json', {
+interface Context {
+  stream: string;
+}
+
+let logger: (data: string) => Promise<void> = () => Promise.resolve();
+
+const createLogger = (ctx: Context) => async (data: string): Promise<void> => {
+  const response = await fetch(`http://localhost:5080/api/org1/${ctx.stream}/_json`, {
     method: 'POST',
     headers: {
       'Authorization': 'Basic ' + Buffer.from('root@example.com:Complexpass#123').toString('base64'),
@@ -21,13 +27,13 @@ async function send_log(data: string): Promise<void> {
   });
 }
 
-async function start_task(id: number, duration: number): Promise<void> {
+async function start_task(id: number, duration: number, ctx: Context): Promise<void> {
   console.log(`New task: id=${id}, duration=${duration}`);
-  const response = await fetch(`http://localhost:5679/webhook/start-task?id=${id}&duration=${duration}`);
+  const response = await fetch(`http://localhost:5679/webhook/start-task?id=${id}&duration=${duration}&stream=${ctx.stream}`);
   const responseBody = await response.text();
   console.log(`Response for id=${id}, duration=${duration}:`);
   console.log(responseBody);
-  await send_log(responseBody);
+  await logger(responseBody);
 }
 
 async function wait_for_background_tasks(): Promise<void> {
@@ -36,14 +42,14 @@ async function wait_for_background_tasks(): Promise<void> {
   console.log('All background tasks finished');
 }
 
-async function send_requests(startId: number = 1): Promise<void> {
+async function send_requests(startId: number = 1, ctx: Context): Promise<void> {
   let id = startId;
 
   while (!terminate) {
-    const duration = getRandomNumber(2, 5);
-    const pause = getRandomNumber(0, 1);
+    const duration = getRandomNumber(10, 15);
+    const pause = getRandomNumber(1, 1);
 
-    const child = start_task(id, duration);
+    const child = start_task(id, duration, ctx);
     backgroundTasks.push(child);
 
     id++;
@@ -66,7 +72,14 @@ rl.on('line', () => {
 var args = process.argv.slice(2);
 
 const startId = args[0] ? Number(args[0]) : 1;
+const stream = args[1] ? args[1] : "stream1";
 
-await send_requests(startId);
+const ctx: Context = {
+  stream
+}
+
+logger = createLogger(ctx);
+
+await send_requests(startId, ctx);
 
 exit(0);
